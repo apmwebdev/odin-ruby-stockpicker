361f58ca49739d2e0267558aa2e5ef43259a2735
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-09 09:29:18 -0800

    Add stock picker functionality

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,1 +1,19 @@
+def stock_picker(stocks)
+  result = [0, 0]
+  stocks.each_with_index do |stock, day_1_index|
+    i = day_1_index + 1
+    while i < stocks.length do
+      if stocks[i] - stocks[day_1_index] > stocks[result[1]] -
+        stocks[result[0]]
+        result[0] = day_1_index
+        result[1] = i
+      end
+      i += 1
+    end
+  end
+  puts result.to_s
+end
 
+input_stocks = [17,3,6,9,15,8,6,1,10,18]
+
+stock_picker(input_stocks)
